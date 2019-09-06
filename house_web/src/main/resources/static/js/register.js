23074a249884020b6b488b4698c3b2ad36a5ff1e
wangliwei <wlw1156@163.com>
2019-09-06 15:46:52 +0800

    change static file

---
+++ house_web/src/main/resources/static/js/register.js
@@ -0,0 +1,42 @@
+// 检查是否为空
+function isEmpty( id ) {
+    var temp = $('[name='+id+']').val();
+    if (temp=="") {
+        console.log(id+" is empty");
+        $('#'+id).addClass('has-error');
+        $('[name='+id+']').focus();
+        return true;
+    }
+    else {
+        $('#'+id).removeClass('has-error');
+        return false;
+    }
+}
+
+function isPswEquel(){
+    var password = $('[name=password]').val();
+    var confirmpsw = $('[name=confirmpsw]').val();
+    if (password!=confirmpsw){
+        $('#confirmpsw').addClass('has-error');
+        $('[name=confirmpsw]').focus();
+        $('.warning').text("两次密码不一致");
+        return false;
+    }
+    else{
+        return true;
+    }
+}
+
+$(document).ready(function () {
+    $("#register_form").on("submit",function (e) {
+        e.preventDefault();
+        // ajax向后端传值，取到data
+        var Emp = isEmpty('username') || isEmpty('address') || isEmpty('tele') || isEmpty('email') || isEmpty('password') || isEmpty('confirmpsw');
+        if(!Emp && isPswEquel()){
+            var details = $("#loginForm").serialize();
+            $.post('#', details, function(data){
+                $('.warning').text(data);
+            });
+        }
+    });
+});
