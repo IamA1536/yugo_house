37fff2edd764cabeb60868b97fd013c4aa6ca553
wangliwei <wlw1156@163.com>
2019-09-10 02:34:13 +0800

    添加了基本样式 为申请加入百度地图定位 修改了首页样式、增加了搜索的js

---
+++ house_web/src/main/resources/static/js/baiduPosition.js
@@ -0,0 +1,17 @@
+// 百度地图API功能
+var map = new BMap.Map("allmap");
+var point = new BMap.Point(116.331398,39.897445);
+map.centerAndZoom(point,12);
+
+var geolocation = new BMap.Geolocation();
+geolocation.getCurrentPosition(function(r){
+    if(this.getStatus() == BMAP_STATUS_SUCCESS){
+        var mk = new BMap.Marker(r.point);
+        map.addOverlay(mk);
+        map.panTo(r.point);
+        $("#bdPosition").text( r.point.lng+','+r.point.lat );
+    }
+    else {
+        alert('failed'+this.getStatus());
+    }
+},{enableHighAccuracy: true})
--- house_web/src/main/resources/static/js/changeUserIcon.js
+++ house_web/src/main/resources/static/js/changeUserIcon.js
@@ -1,3 +1,55 @@
 $(document).ready(function(){
+    //隐藏修改头像框
+    $(".black_all").hide();
+    //点击修改头像
+    $(".my_icon").on("click",function () {
+    //    显示修改头像框
+        $(".black_all").show();
+    });
 
-}
+    //选择图片，头像预览改变
+    $("#choose_user_icon").on("change",function(){
+        var fileTypes = [".jpg",".png"];
+        var filePath = this.value;
+        if(filePath) {
+            //获取文件
+            var file = $("#user_icon_form").find("input")[0].files[0];
+
+            //创建读取文件的对象
+            var reader = new FileReader();
+
+            //创建文件读取相关的变量
+            var imgFile;
+
+            //为文件读取成功设置事件
+            reader.onload = function (e) {
+                // alert('文件读取完成');
+                imgFile = e.target.result;
+                console.log(imgFile);
+                $("#user_icon_in_form").attr('src', imgFile);
+            };
+
+            //正式读取文件
+            reader.readAsDataURL(file);
+        }
+        // else{
+        //         alert('请选择图片，类型为jpg或png');
+        //         file.value = '';
+        // }
+        else{
+             alert('请选择图片，类型为jpg或png');
+        }
+    });
+//    点击提交隐藏遮罩和修改头像框，直接在前端改变头像
+//    后端没写，阻拦了表单提交
+    $("#submit_icon").on("click",function (e) {
+        e.preventDefault();
+        var imgFile = $("#user_icon_in_form").attr('src');
+        $(".my_icon").attr('src', imgFile);
+        $(".black_all").hide();
+    });
+
+    $("#remove").on("click",function () {
+        $(".black_all").hide();
+    });
+});
