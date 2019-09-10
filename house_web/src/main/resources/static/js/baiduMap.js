344bce59f8fbe23144dce979cb1938d0b092e196
wangliwei <wlw1156@163.com>
2019-09-10 10:41:30 +0800

    给周边信息添加maker 修改itemlist搜索框

--- house_web/src/main/resources/static/js/baiduMap.js
+++ house_web/src/main/resources/static/js/baiduMap.js
@@ -1,10 +1,18 @@
 var mp = new BMap.Map("baidu_map");
-mp.centerAndZoom(new BMap.Point(116.3964, 39.9093), 10);
+var point = new BMap.Point(116.3964, 39.9093);
+mp.centerAndZoom(point, 10);
 mp.enableScrollWheelZoom();
 
 var canvasLayer = new BMap.CanvasLayer({
     update: update
 });
+
+var marker = new BMap.Marker(point);  // 创建标注
+mp.addOverlay(marker);              // 将标注添加到地图中
+mp.panTo(new_point);
+
+var geolocation = new BMap.Geolocation();
+$("#bdPosition").text(r.point.lng + ',' + r.point.lat);
 
 function update() {
     var ctx = this.canvas.getContext("2d");
@@ -18,9 +26,9 @@ function update() {
     var temp = {};
     ctx.fillStyle = "rgba(50, 50, 255, 0.7)";
     ctx.beginPath();
-    var data = [
-        new BMap.Point(116.297047, 39.979542),
-    ];
+//     var data = [
+//         new BMap.Point(116.297047, 39.979542),
+//     ];
 
     for (var i = 0, len = data.length; i < len; i++) {
         var pixel = mp.pointToPixel(data[i]);
